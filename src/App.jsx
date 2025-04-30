7ce6aa30d250ab25aa5f9dd18785655884784a69
CharaBJ <narutoespro2@gmail.com>
2025-04-30 13:49:25 -0700

    BRR BRR PATAPIM (LAST)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import React, {useState} from 'react'
+import './App.css'
 
+import AnimalsShow from './AnimalsShow'
+
+function getRandomAnimal(){
+  const animals = ['cat', 'bird', 'cow', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() = animals.length)]
+}
 
 function App() {
+  const [animals, setAnimals] = useState([])
 
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+  
+  const renderedAnimals = animals.map((animal, index) => {
+    return<AnimalsShow type ={animal} key = {index}/>
+  })
 
   return (
-    <>
-    <h1>Animals</h1>
-    </>
+    <div className='app'>
+      <button on onClick={handleClick}>Add Animal</button>
+      <div className='animals-list'>
+        {renderedAnimals}
+      </div>
+    </div>
   )
+
 }
 
 export default App
